5cbc3736ca13dba82b091c9cdba0474f0f49a341
k1au3 <lindquistconcern@gmail.com>
2023-07-10 17:17:42 +0300

    awards complete

--- src/components/award.js
+++ src/components/award.js
@@ -3,7 +3,8 @@ import { arrowForward, star } from "ionicons/icons";
 import Button from "./button";
 import large from '../images/roberto-nickson-rEJxpBskj3Q-unsplash.jpg';
 import small from '../images/lissete-laverde-7jOeRc0dOWs-unsplash.jpg';
-import reviewer from '../images/reviews/rev4.jpeg'
+import reviewer from '../images/reviews/rev4.jpeg';
+import signature from '../images/icons/signature.png';
 
 
 
@@ -43,7 +44,7 @@ const Awards = () => {
                 <p>
                     We are a residential Interior design firm located in Hurlingham. Our boutique studio offers more than 20 years experience in custom renovations, new homw finishes and furniture designs. We can help with all aspects of your project from design concepts and putting together the construction team, to sourcing furniture and art. While also tending to the thousands of details it takes to create a custom home tailored to your lifestyle.
                 </p>
-                <img src="" alt="signature" />
+                <img src={signature} alt="signature" />
                 <h4>Jennie Wambui, CEO</h4>
                 <Button className="btn">Read More <IonIcon icon={arrowForward} /></Button>
             </aside>
